e58745765f5614a355cb04c7e739ec33f3bd7153
Christian Abildsø <cabildso@cisco.com>
2013-10-18 14:15:58 +0200

    Initial commit

---
+++ fsharpers/BiggestNum.fs
@@ -0,0 +1,14 @@
+﻿module BiggestNum
+
+open System
+
+let getMax list = list |> max
+
+let getMax2 list =
+    let rec getMax2' max list =
+        match list with
+        | head::tail -> tail |> getMax2' (if max >= head then max else head)
+        | []         -> max
+    list |> List.tail |> getMax2' (list |> List.head)
+
+let getMax3 list = list |> List.fold (fun max item -> if item > max then item else max) (list |> List.head)
---
+++ fsharpersTests/BiggestNumTest.fs
@@ -0,0 +1,10 @@
+﻿module BiggestNumTest
+
+open FsUnit
+open NUnit.Framework
+open BiggestNum
+
+[<Test>]
+let ``Gets biggest number``() =
+    [2;4;9;14;9;3;2] |> getMax2 |> should equal 14
+
